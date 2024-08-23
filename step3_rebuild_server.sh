d0f13c219b89f3c80000e58cfb2653bcfd635abd
Antony Holmes <antony@antonyholmes.dev>
2024-08-23 10:54:41 -0400

    fix: renamed and simplified build process

--- step3_rebuild_server.sh
+++ step3_rebuild_server.sh
@@ -1,8 +1,8 @@
 # generate keys if they don't exist
 if [[ ! -f "jwtRS256.key" ]]
 then
-	./go-edb-api/create_rsa_keys.sh
+	./create_rsa_keys.sh
 fi
 
 # rebuild in background then replace once built
-docker-compose up -d --no-deps --build edb-web-app
+docker-compose up -d --no-deps --build edb-server
