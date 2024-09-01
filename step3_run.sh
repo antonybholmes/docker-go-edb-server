014ea4c6db3b6e06c4c1bc89a278cae9db1c540e
Antony Holmes <antony@antonybholmes.com>
2024-09-01 18:29:22 -0400

    fix: Bug fixes and updates.

--- step3_run.sh
+++ step3_run.sh
@@ -2,5 +2,23 @@
 #docker-compose down
 #docker image rm docker-rust-edb-api-web
 #docker-compose up -d
-./step1_rebuild_server.sh
-./step2_rebuild_nginx.sh
+
+# generate keys if they don't exist
+if [[ ! -f "jwtRS256.key" ]]
+then
+	./create_rsa_keys.sh
+fi
+
+# start redis
+docker-compose --profile redis up -d --no-deps --build
+
+# start mail service
+docker-compose build --no-cache edb-server-mailer
+docker-compose --profile mailer up -d --no-deps --build
+
+# start the server
+docker-compose build --no-cache edb-server
+docker-compose up -d --no-deps --force-recreate edb-server
+
+# start nginx
+docker-compose up -d --no-deps --build nginx
