f40a80996b68140cd540e787af15045ac6dff5e2
Antony Holmes <antony@antonybholmes.com>
2024-12-18 22:16:57 -0500

    fix: Bug fixes and updates.

--- step1_update.sh
+++ step1_update.sh
@@ -4,19 +4,19 @@ mkdir -p src
 
 cd src
 
-git clone https://github.com/antonybholmes//go-edb-server.git
-git clone https://github.com/antonybholmes//go-edb-server-mailer.git
-git clone https://github.com/antonybholmes//go-dna.git
-git clone https://github.com/antonybholmes//go-genes.git
-git clone https://github.com/antonybholmes//go-auth.git
-git clone https://github.com/antonybholmes//go-basemath.git
-git clone https://github.com/antonybholmes//go-sys.git
-git clone https://github.com/antonybholmes//go-mailer.git
-git clone https://github.com/antonybholmes//go-mutations.git
-git clone https://github.com/antonybholmes//go-geneconv.git
-git clone https://github.com/antonybholmes//go-pathway.git
-git clone https://github.com/antonybholmes//go-motifs.git
-git clone https://github.com/antonybholmes//go-gex.git
+git clone git@github.com:antonybholmes/go-edb-server.git
+git clone git@github.com:antonybholmes/go-edb-server-mailer.git
+git clone git@github.com:antonybholmes/go-dna.git
+git clone git@github.com:antonybholmes/go-genes.git
+git clone git@github.com:antonybholmes/go-auth.git
+git clone git@github.com:antonybholmes/go-basemath.git
+git clone git@github.com:antonybholmes/go-sys.git
+git clone git@github.com:antonybholmes/go-mailer.git
+git clone git@github.com:antonybholmes/go-mutations.git
+git clone git@github.com:antonybholmes/go-geneconv.git
+git clone git@github.com:antonybholmes/go-pathway.git
+git clone git@github.com:antonybholmes/go-motifs.git
+git clone git@github.com:antonybholmes/go-gex.git
 
 pwd=`pwd`
 for d in `find ${pwd} -maxdepth 1 -mindepth 1 -type d`
