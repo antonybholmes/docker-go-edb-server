8741239c75bafb0065af8fb2fb679c9b21107b07
Antony Holmes <antony@antonyholmes.dev>
2026-06-09 13:54:09 -0400

    Fixed: Bug fixes and updates.

--- step1_update.sh
+++ step1_update.sh
@@ -1,36 +1,36 @@
 git pull
 
-mkdir -p src
+# mkdir -p src
 
-cd src
+# cd src
 
-git clone git@github.com:antonybholmes/go-mailserver.git
-git clone git@github.com:antonybholmes/go-edbserver-gin.git
-git clone git@github.com:antonybholmes/go-edbmailserver.git
-git clone git@github.com:antonybholmes/go-dna.git
-git clone git@github.com:antonybholmes/go-genome.git
-git clone git@github.com:antonybholmes/go-web.git
-git clone git@github.com:antonybholmes/go-basemath.git
-git clone git@github.com:antonybholmes/go-sys.git
-git clone git@github.com:antonybholmes/go-wgs.git
-git clone git@github.com:antonybholmes/go-geneconv.git
-git clone git@github.com:antonybholmes/go-pathway.git
-git clone git@github.com:antonybholmes/go-motifs.git
-git clone git@github.com:antonybholmes/go-gex.git
-git clone git@github.com:antonybholmes/go-seqs.git
-git clone git@github.com:antonybholmes/go-beds.git
-git clone git@github.com:antonybholmes/go-cytobands.git
-git clone git@github.com:antonybholmes/go-hubs.git
-git clone git@github.com:antonybholmes/go-scrna.git
+# git clone git@github.com:antonybholmes/go-mailserver.git
+# git clone git@github.com:antonybholmes/go-edbserver-gin.git
+# git clone git@github.com:antonybholmes/go-edbmailserver.git
+# git clone git@github.com:antonybholmes/go-dna.git
+# git clone git@github.com:antonybholmes/go-genome.git
+# git clone git@github.com:antonybholmes/go-web.git
+# git clone git@github.com:antonybholmes/go-basemath.git
+# git clone git@github.com:antonybholmes/go-sys.git
+# git clone git@github.com:antonybholmes/go-wgs.git
+# git clone git@github.com:antonybholmes/go-geneconv.git
+# git clone git@github.com:antonybholmes/go-pathway.git
+# git clone git@github.com:antonybholmes/go-motifs.git
+# git clone git@github.com:antonybholmes/go-gex.git
+# git clone git@github.com:antonybholmes/go-seqs.git
+# git clone git@github.com:antonybholmes/go-beds.git
+# git clone git@github.com:antonybholmes/go-cytobands.git
+# git clone git@github.com:antonybholmes/go-hubs.git
+# git clone git@github.com:antonybholmes/go-scrna.git
 
-pwd=`pwd`
-for d in `find ${pwd} -maxdepth 1 -mindepth 1 -type d`
-do
-	echo ${d}
-	cd ${d}
-	./pull.sh ${msg}
-	cd ${pwd}
-done
+# pwd=`pwd`
+# for d in `find ${pwd} -maxdepth 1 -mindepth 1 -type d`
+# do
+# 	echo ${d}
+# 	cd ${d}
+# 	./pull.sh ${msg}
+# 	cd ${pwd}
+# done
 
 # for storing trace data
 
